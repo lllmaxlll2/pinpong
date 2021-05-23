99d6877fa3484059b54d268a7fe4fc9a4a849cc8
Max <84273155+lllmaxlll2@users.noreply.github.com>
2021-05-23 17:25:19 +0500

    Update my_memory_card.py

--- my_memory_card.py
+++ my_memory_card.py
@@ -36,5 +36,38 @@ FPS = 60
 racket1 = Player("racket.png", 30, 200, 4, 50, 150)
 racket2 = Player("racket.png", 520, 200, 4, 50, 150)
 ball = GameSprite('tenis_ball.jpg', 200, 200, 4, 50, 50)
-                
+
+font.init()
+font = font.font(None, 35)
+lose1 = font.render("PLAYER 1 LOSE!", True, (180, 0, 0))
+lose2 = font.render("PLAYER 2 LOSE!", True, (180, 0, 0))
+
+speed_x = 3
+speed_y = 3
+
+while game:
+    for e in event.get():
+        if e_type == QUIT:
+            game = False
+    if finish != True:
+        racket1.update_l()
+        racket2.update_r()
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+        if sprite.collde_rect(racket1, ball) or sprite.collide_rect(racket2, ball):
+            speed_x *= -1
+            speed_y = 1
+        if ball.rect.y > win_height -50 or ball.rect.y < 0:
+            speed_y*= -1
+        if ball.rect.x < 0:
+            finish = True
+            window.blit(lose1, (200,200))
+            game_over = True
+        if ball.rect.x > win_wigth:
+            finish = True
+            window.blit(lose2, (200,200))
+            game_over = True            
+        racket1.reset()
+        racket2.reset()
+        ball.reset()
         
